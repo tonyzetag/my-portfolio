d6f6ff42b9dce9430dde54f22dfad88585bd0dab
Jose Antonio <tonyzetag@outlook.com>
2024-09-01 03:28:38 +0200

    chore: Update favicon.ico link in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/z-favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </head>
       <body>
